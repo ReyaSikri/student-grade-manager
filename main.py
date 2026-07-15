b1d68d102a5a91771852907035ac2c818b314b2a
Reya Sikri <sikrireya@gmail.com>
2026-07-15 13:04:24 +0530

    Add age verification and voting eligibility

--- main.py
+++ main.py
@@ -1,4 +1,16 @@
 print("=" * 40)
 print("Welcome to student grade manager")
 print("=" * 40)
-git status
+
+name = input ("Enter your name:")
+print(f"Hey {name}!")
+print ("Welcome to student grade manager")
+
+age = int(input("Enter your age:"))
+
+if age >=18:
+    print ("You are an adult!")
+    print("You are eligible to vote")
+else:
+    print ("You are not an adult")
+    print("You are not eligable to vote")
